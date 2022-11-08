1ff71daf8f14b698e8f59d85735890abded256e9
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-11-08 15:13:41 +0100

    Changes

---
+++ shell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments as array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,7 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#endif /* SHELL_H */
