7b790f98b123b17d919fb9de017502782570f35b
Adeosun Oyewale Oluwapelumi <68809636+waley-code@users.noreply.github.com>
2022-11-18 11:40:27 +0000

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,19 +1,44 @@
 #include "shell.h"
 
 /**
- * main - Entry point
- * @argc: argument count
- * @argv: arguments as array
- * Return: Always 0
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-	(void)argv;
-	(void)argc;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	signal(SIGINT, controlC);
-	prompt();
-	return (0);
-	return (0);
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
